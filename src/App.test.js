3f7d3c9fa9237c1e8ab97608bf8c5025af8e91c3
David Schurer <david.schurer@yahoo.com>
2024-05-06 21:45:12 -0700

    more updates

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import ToDoListMaker from './ToDoListMaker';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<ToDoListMaker />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/ToDoListMaker.js
+++ src/ToDoListMaker.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import './App.css';
+import './ToDoListMaker.css';
 
 function App() {
   const [toDoTask, setToDoTask] = useState('');
