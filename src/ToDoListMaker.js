64d6a21d3509f84ae97ca999cdf42eede99ce1e0
David Schurer <david.schurer@yahoo.com>
2024-05-07 23:36:32 -0700

    update

--- src/ToDoListMaker.js
+++ src/ToDoListMaker.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import './ToDoListMaker.css';
+import ToDoListPic from './toDoListIconPNG.png';
 
 function App() {
   const [toDoTask, setToDoTask] = useState('');
@@ -41,7 +42,8 @@ function App() {
 
   const handleSaveListConfirm = () => {
     setisModalOpen(false);
-    setSavedLists([...savedLists, {name: listName, tasks: editedTasks}]);
+    const newList = { name: listName, tasks: editedTasks};
+    setSavedLists([...savedLists, newList]);
     setToDoList([]);
     setToDoTask('');
     setListName('');
@@ -70,76 +72,81 @@ function App() {
 
   return (
     <>
-    <div className="ToDoListHome">
-          <h1>To-Do List Maker</h1>
-          <button onClick={handleSaveList}>Save List</button>
-          <form onSubmit={handleSubmit}>
-            <p>Please Enter A Task:</p>
-            <textarea
-              onChange={handleInputChange}
-              maxLength={500}
-              rows={10}
-              cols={80}
-              value={toDoTask}
-            />
-            <button type="submit">Add Task</button>
-          </form>
-          {isListOpen && (
-          <ol>
-            {toDoList.map((toDoTask, index) => (
-              <li key={index}>
-                  <input
-                   type="text"
-                   value={editedTasks[index] || toDoTask}
-                   onChange={(event) => handleEditInputChange(event, index)}
-                   onBlur={() => handleEditTask(index)}
-                   />
-              </li>
-            ))}
-          </ol>
-        )}
-        {!isListOpen && toDoList.length > 0 && (
-          <button onClick={handleOpenList}>Open List</button>
-        )}
-          <button onClick={handleCloseList}>Close List</button>
-    </div>
+    <div className="ToDoListContainer">
+      <div className="ToDoListContent">
+      <img src={ToDoListPic} alt="ToDoListPic" style={{ width: '200px', height: '200px' }} className="ToDoListPic"></img>
+        <div className="ToDoListHome">
+              <h1>To-Do List Maker</h1>
+              <button onClick={handleSaveList}>Save List</button>
+              <form onSubmit={handleSubmit}>
+                <p>Please Enter A Task:</p>
+                <textarea
+                  onChange={handleInputChange}
+                  maxLength={500}
+                  rows={10}
+                  cols={80}
+                  value={toDoTask}
+                />
+                <button type="submit">Add Task</button>
+              </form>
+              {isListOpen && (
+              <ol>
+                {toDoList.map((toDoTask, index) => (
+                  <li key={index}>
+                      <input
+                      type="text"
+                      value={editedTasks[index] || toDoTask}
+                      onChange={(event) => handleEditInputChange(event, index)}
+                      onBlur={() => handleEditTask(index)}
+                      />
+                  </li>
+                ))}
+              </ol>
+            )}
+            {!isListOpen && toDoList.length > 0 && (
+              <button onClick={handleOpenList}>Open List</button>
+            )}
+              <button onClick={handleCloseList}>Close List</button>
+        </div>
 
-    {isModalOpen && (
-      <div className="modal">
-          <div className="modal-content">
-            <span className="close" onClick={handleModalClose}>
-                &times;
-            </span>
-            <p>Please Enter The Name For Your ToDo List:</p>
-            <input
-              type="text"
-              value={listName}
-              onChange={(e) => setListName(e.target.value)}
-            />
-            <button onClick={handleSaveListConfirm}>OK</button>
+        {isModalOpen && (
+          <div className="modal">
+              <div className="modal-content">
+                <span className="close" onClick={handleModalClose}>
+                    &times;
+                </span>
+                <p>Please Enter The Name For Your To-Do List:</p>
+                <input
+                  type="text"
+                  value={listName}
+                  onChange={(e) => setListName(e.target.value)}
+                />
+                <button onClick={handleSaveListConfirm}>OK</button>
+              </div>
           </div>
-      </div>
-    )}
+        )}
 
-    <div className="SavedLists">
-      <h2>Saved To-Do Lists:</h2>
-      {savedLists.map((list, index) => (
-        <button key={index} onClick={() => setToDoList(list.tasks)}>
-          {list.name}
-        </button>
-      ))}
-    </div>
-
-    <div className="DeleteList">
-      <h2>Delete A To-Do-List</h2>
-      <p>Select A To-Do List To Delete:</p>
-        <div className="grid-container">
+        <div className="SavedLists">
+          <h2>Saved To-Do Lists:</h2>
           {savedLists.map((list, index) => (
-            <button key={index} onClick={() => handleDeleteList(index)}>
+            <button key={index} onClick={() => setToDoList(list.tasks)}>
               {list.name}
             </button>
           ))}
         </div>
+
+        <div className="DeleteList">
+          <h2>Delete A To-Do-List</h2>
+          <p>Select A To-Do List To Delete:</p>
+            <div className="grid-container">
+              {savedLists.map((list, index) => (
+                <button key={index} onClick={() => handleDeleteList(index)}>
+                  {list.name}
+                </button>
+              ))}
+            </div>
+        </div>
+      </div>
     </div>
     </>
   );
