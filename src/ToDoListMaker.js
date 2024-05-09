c9483bb7d37c61fb6501c56db3e780b4e64d784e
David Schurer <david.schurer@yahoo.com>
2024-05-08 21:18:41 -0700

    comments

--- src/ToDoListMaker.js
+++ src/ToDoListMaker.js
@@ -29,58 +29,67 @@ function App() {
 
   // --------------------------------[Event Handlers]--------------------------------
 
+  // Handles updating to-do list task input
   const handleInputChange = (event) => {
     setToDoTask(event.target.value);
   };
 
+  // Handles updating edited to-do list tasks
   const handleEditInputChange = (event, index) => {
     const updatedTasks = [...editedTasks];
     updatedTasks[index] = event.target.value;
     setEditedTasks(updatedTasks);
   };
 
-
+  // Handles submitting a new to-do list task
   const handleSubmit = (event) => {
     event.preventDefault();
-    setEditedTasks([...editedTasks, toDoTask]);
-    setToDoList([...toDoList, toDoTask]);
-    setToDoTask('');
+    setEditedTasks([...editedTasks, toDoTask]); // New task will be added to editedTasks
+    setToDoList([...toDoList, toDoTask]); // New task added to toDoList
+    setToDoTask(''); // Clears the task input
   };
 
+  // Handles opening modal to save list
   const handleSaveList = () => {
     setisModalOpen(true);
   };
 
+  // Handles closing the modal
   const handleModalClose = () => {
     setisModalOpen(false);
   };
 
+  // handles confirming and saving a to-do list
   const handleSaveListConfirm = () => {
     setisModalOpen(false);
     const newList = { name: listName, tasks: editedTasks };
     setSavedLists([...savedLists, newList]);
-    setToDoList([]);
-    setToDoTask('');
-    setListName('');
-    setEditedTasks([]);
+    setToDoList([]); // Clears the current to-do list
+    setToDoTask(''); // clears the task input
+    setListName(''); // Clears the list name input
+    setEditedTasks([]); // Clears the edited tasks
   };
 
+  // Handles deleting a saved to-do list
   const handleDeleteList = (index) => {
     const updatedLists = [...savedLists];
     updatedLists.splice(index, 1);
     setSavedLists(updatedLists);
   };
 
+  // Handles editing a task
   const handleEditTask = (index) => {
     const updatedList = [...toDoList];
     updatedList[index] = editedTasks[index];
     setToDoList(updatedList);
   };
 
+  // Handles closing the current to-do list
   const handleCloseList = () => {
     setIsListOpen(false);
   };
 
+  // Handles opening the current to-do list
   const handleOpenList = () => {
     setIsListOpen(true);
   };
@@ -94,6 +103,7 @@ function App() {
             <h1>To-Do List Maker</h1>
             <form onSubmit={handleSubmit}>
               <p><strong>Please Enter A Task:</strong></p>
+              {/* Textarea used for entering a task to add to the to-do list */}
               <textarea
                 onChange={handleInputChange}
                 maxLength={500}
@@ -102,14 +112,18 @@ function App() {
                 value={toDoTask}
               />
               <div className="buttonContainer">
+                {/* Button to add a new task*/}
                 <button type="submit"><strong>Add Task</strong></button>
+                {/* Button to save the to-do list (opens up a modal)*/}
                 <button onClick={handleSaveList}><strong>Save List</strong></button>
               </div>
             </form>
+            {/* Displays the current to-do list tasks */}
             {isListOpen && (
               <ol>
                 {toDoList.map((toDoTask, index) => (
                   <li key={index}>
+                    {/* Input field for the tasks, allowing users to edit tasks as they add them */}
                     <input
                       type="text"
                       value={editedTasks[index] || toDoTask}
@@ -120,12 +134,15 @@ function App() {
                 ))}
               </ol>
             )}
+            {/* Button to open and display current to-do list */}
             {!isListOpen && toDoList.length > 0 && (
               <button onClick={handleOpenList} className="OpenListButton"><strong>Open List</strong></button>
             )}
+            {/* Button to close and stop displaying current to-do list */}
             <button onClick={handleCloseList} className="CloseListButton"><strong>Close List</strong></button>
           </div>
 
+          {/* Modal for entering name for saving to-do list */}
           {isModalOpen && (
             <div className="modal">
               <div className="modal-content">
@@ -133,16 +150,19 @@ function App() {
                   &times;
                 </span>
                 <p><strong>Please Enter The Name For Your To-Do List:</strong></p>
+                {/* Text input field for entering a name for saved to-do list */}
                 <input
                   type="text"
                   value={listName}
                   onChange={(e) => setListName(e.target.value)}
                 />
+                {/* Button for confirming and saving to-do list */}
                 <button onClick={handleSaveListConfirm}>OK</button>
               </div>
             </div>
           )}
 
+          {/* Displays all of the saved to-do lists as buttons */}
           <div className="SavedListsContainer">
             <div className="SavedLists">
               <h2>Saved To-Do Lists:</h2>
@@ -155,7 +175,8 @@ function App() {
               ))}
             </div>
 
-
+            {/* Displays the names of all of the saved to-do lists as buttons, 
+                user is able to click them to delete those to-do lists */}
             <div className="DeleteList">
               <h2>Delete To-Do List:</h2>
               <p><strong>Select A To-Do List To Delete:</strong></p>
